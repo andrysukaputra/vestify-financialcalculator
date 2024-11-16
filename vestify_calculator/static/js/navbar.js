5766e3f545733d585fd1c6f7119cdb09cf79bae5
andrysukaputra <andrysukaputra@gmail.com>
2024-11-16 19:52:36 +0700

    Alpha 08:  adding new navbar, deleting navbar.js, deleting features.html

--- vestify_calculator/static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("toggleNavbar").addEventListener("click", function () {
-  this.classList.toggle("collapsed");
-  var sidebar = document.getElementById("sidebar");
-  if (sidebar.style.display === "none" || sidebar.style.display === "") {
-    sidebar.style.display = "block";
-  } else {
-    sidebar.style.display = "none";
-  }
-});
